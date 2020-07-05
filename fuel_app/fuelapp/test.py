97e608f05dedec606ea33e2682da543a94e31844
Mehandeis <67025415+Mehandeis@users.noreply.github.com>
2020-07-05 17:52:29 -0500

    Update test.py

--- fuel_app/fuelapp/test.py
+++ fuel_app/fuelapp/test.py
@@ -82,36 +82,28 @@ class FlaskTestCases(unittest.TestCase):
         #Profile Input Test Cases
     def test_correct_error_message_registration_invalid_fullname_short(self):
         tester=app.test_client(self)
-        response=tester.post('/register',data=dict(username="u"),follow_redirects=True)
-        self.assertIn(b'Field must be between 2 and 25 characters long.', response.data)
+        response=tester.post('/profile',data=dict(fullname="qq"),follow_redirects=True)
+        self.assertIn(b'Field must be between 3 and 50 characters long.', response.data)
     def test_correct_error_message_registration_invalid_fullname_long(self):
         tester=app.test_client(self)
-        response=tester.post('/register',data=dict(username="uuuuuuuuuuuuuuuuuuuuuuuuuuu"),follow_redirects=True)
-        self.assertIn(b'Field must be between 2 and 25 characters long.', response.data)
+        response=tester.post('/profile',data=dict(fullname="qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"),follow_redirects=True)
+        self.assertIn(b'Field must be between 3 and 50 characters long.', response.data)
     def test_correct_error_message_registration_invalid_address1_short(self):
         tester=app.test_client(self)
-        response=tester.post('/register',data=dict(password="12345"),follow_redirects=True)
-        self.assertIn(b'Field must be between 6 and 30 characters long.', response.data)
+        response=tester.post('/profile',data=dict(address1="street"),follow_redirects=True)
+        self.assertIn(b'Field must be between 10 and 100 characters long.', response.data)
     def test_correct_error_message_registration_invalid_address1_long(self):
         tester=app.test_client(self)
-        response=tester.post('/register',data=dict(password="uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"),follow_redirects=True)
-        self.assertIn(b'Field must be between 6 and 30 characters long.', response.data)
-    def test_correct_error_message_registration_invalid_address2_short(self):
-        tester=app.test_client(self)
-        response=tester.post('/register',data=dict(password="uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"),follow_redirects=True)
-        self.assertIn(b'Field must be between 6 and 30 characters long.', response.data)
-    def test_correct_error_message_registration_invalid_address2_long(self):
-        tester=app.test_client(self)
-        response=tester.post('/register',data=dict(password="uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"),follow_redirects=True)
+        response=tester.post('/profile',data=dict(address1"1234567890 streeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet"),follow_redirects=True)
         self.assertIn(b'Field must be between 6 and 30 characters long.', response.data)
     def test_correct_error_message_registration_invalid_city_short(self):
         tester=app.test_client(self)
-        response=tester.post('/register',data=dict(password="uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"),follow_redirects=True)
-        self.assertIn(b'Field must be between 6 and 30 characters long.', response.data)
+        response=tester.post('/profile',data=dict(city="c"),follow_redirects=True)
+        self.assertIn(b'Field must be between 2 and 30 characters long.', response.data)
     def test_correct_error_message_registration_invalid_city_long(self):
         tester=app.test_client(self)
-        response=tester.post('/register',data=dict(password="uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"),follow_redirects=True)
-        self.assertIn(b'Field must be between 6 and 30 characters long.', response.data)
+        response=tester.post('/profile',data=dict(city="cccccciiiiiiiitttttttttyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"),follow_redirects=True)
+        self.assertIn(b'Field must be between 2 and 30 characters long.', response.data)
 
 if __name__ == "__main__":
     unittest.main()
