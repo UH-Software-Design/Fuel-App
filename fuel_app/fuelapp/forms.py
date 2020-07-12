8e01b682097af92e2c0135537de06c222e207284
mortar-and-pestle <chasebowman13@gmail.com>
2020-07-12 12:36:17 -0500

    Tweaking Frontend Validation

--- fuel_app/fuelapp/forms.py
+++ fuel_app/fuelapp/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, HiddenField, SelectField, TextAreaField#, IntegerField, DecimalField
 from wtforms.fields.html5 import DateField, IntegerField, EmailField, DecimalField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange, Optional, Regexp, ValidationError
+from wtforms.widgets import TextInput
 from wtforms.widgets.html5 import NumberInput
 from fuelapp.models import User
 
@@ -29,9 +30,9 @@ class loginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class quoteForm(FlaskForm):
-    gallonsRequested = IntegerField('Gallons requested: ', validators = [DataRequired(), NumberRange(min = 50, max = 3500)], widget=NumberInput(min = 0, max = 3501))
-    deliveryDate = DateField('Delivery date', format = '%Y-%m-%d', validators = [DataRequired(message="You need to enter a date")])
-    deliveryAddress = TextAreaField('Delivery address: ', validators = [DataRequired("Update your profile with an address")])
+    gallonsRequested = IntegerField('Gallons Requested: ', validators = [DataRequired(), NumberRange(min = 50, max = 3500)], widget=NumberInput(min = 0, max = 3501))
+    deliveryDate = DateField('Delivery Date:', format = '%Y-%m-%d', validators = [DataRequired(message="You need to enter a date")])
+    deliveryAddress = StringField('Delivery address: ', validators = [DataRequired("Update your profile with an address")])
     rate = DecimalField("Price per Gallon:", validators = [Optional()])
     total = DecimalField("Total Amount:", validators = [Optional()])
     submit = SubmitField(label="Submit")
@@ -43,5 +44,5 @@ class profileForm(FlaskForm):
     address2 = StringField('Address 2: ', validators = [Optional()])
     city = StringField('City: ', validators = [DataRequired(), Length(min=2, max =30)])
     state = SelectField ('State: ', validators = [DataRequired()],choices = [('TX', 'Texas'), ('CA', 'California'), ('NY', 'New York')])
-    zipcode = IntegerField('Zip Code: ', validators= [DataRequired()])#, Length(min=5,max=5)
+    zipcode = IntegerField('Zip Code: ', validators= [DataRequired(), Length(min=5,max=5)], widget=NumberInput(min = 501, max = 99950))
     submit = SubmitField('Submit')
